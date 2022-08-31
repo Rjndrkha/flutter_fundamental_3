3e0a6b9400288571776acabce387e3517ae03f52
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-08-31 19:27:03 +0700

    done all

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # flutter_fundamental_3
 
-A new Flutter project.
+#Rajendra Rakha A P
 
-## Getting Started
+Praktikum 1: Menerapkan Gesture Detector
 
-This project is a starting point for a Flutter application.
+- On Tap: Mengklik hanya satu kali
+![Praktikum1_OnTap](images/Praktikum1_OnTap.png)
+- On Double Tap: Mengklik 2 kali
+![Praktikum1_OnDoubleTap](images/Praktikum1_OnDoubleTap.png)
+- On Long Press: Menahan beberapa detik
+![Praktikum1_OnLongPress](images/Praktikum1_OnLongPress.png)
 
-A few resources to get you started if this is your first Flutter project:
+Praktikum 2: Menerapkan Input Widget dan Forms
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- Validasi digunakan dalam form , jika form tidak sesuai validasi akan diberikan peringatan / pemberitahuan
+![Praktikum2_1](images/Praktikum2_1.png)
+![Praktikum2_2](images/Praktikum2_2.png)
+![Praktikum2_3](images/Praktikum2_3.png)
+![Praktikum2_4](images/Praktikum2_4.png)
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Praktikum 3: Menerapkan Custom Input dan FormField Widget
+
+- Custom input ini hanya memperbolehkan angka 1-9 saja untuk diinputkan karena terdapat validasi yang mengatur jika input max 9 dan untuk text adalah 9 
+
+![Praktikum3_1](images/Praktikum3_1.png)
+![Praktikum3_2](images/Praktikum3_2.png)
+![Praktikum3_3](images/Praktikum3_3.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental_3/forms.dart';
+import 'package:flutter_fundamental_3/image_widget.dart';
+import 'package:flutter_fundamental_3/input_fields.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,6 +52,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final TextEditingController _controller = TextEditingController.fromValue(
+      const TextEditingValue(text: "isi angka saja"));
 
   void _incrementCounter() {
     setState(() {
@@ -78,38 +83,50 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: GestureDetector(
+          onLongPress: _incrementCounter,
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Custom Input & Form Field Widget", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 50,),
+              Form(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  VerificationCodeFormField(controller: _controller),
+                  Builder(
+                    builder: (BuildContext subContext) => ElevatedButton(
+                      onPressed: () {
+                        final valid = Form.of(subContext)?.validate();
+                        if (valid != null) {
+                          print("valid: $valid");
+                        }
+                      },
+                      child: const Text("validate"),
+                    ),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
